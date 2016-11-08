803db7fe9ada73b9c6a7ae428dcec4100e3ca64d
Hugo Caille <hugo.caille@isen-lille.fr>
2016-11-08 19:14:09 +0100

    Add ApplicationInsights to project

---
+++ MatchFM/App_Start/Startup.Hangfire.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Hangfire;
+using Hangfire.Azure.ServiceBusQueue;
+using Hangfire.SqlServer;
+using Microsoft.WindowsAzure;
+using Owin;
+
+namespace MatchFM
+{
+    public partial class Startup
+    {
+        public void ConfigureHangfire(IAppBuilder app)
+        {
+            var sqlStorage = new SqlServerStorage("DefaultConnection");
+            var connectionString = CloudConfigurationManager.GetSetting("Microsoft.ServiceBus.ConnectionString");
+            sqlStorage.UseServiceBusQueues(connectionString);
+
+            GlobalConfiguration.Configuration.UseStorage(sqlStorage);
+            app.UseHangfireDashboard();
+            app.UseHangfireServer();
+        }
+    }
+}
