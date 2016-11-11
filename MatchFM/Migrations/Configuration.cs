c8fee0971534fe5ad3ac9fab611b5d4130618034
Hugo Caille <hugo.caille@isen-lille.fr>
2016-11-11 20:52:12 +0100

    Adding missing files

--- MatchFM/Migrations/Configuration.cs
+++ MatchFM/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace MatchFM.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(MatchFM.Models.ApplicationDbContext context)
