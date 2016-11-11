572dde7d47a9cd0f3c1470077f1055a88085829f
Hugo Caille <hugo.caille@isen-lille.fr>
2016-11-11 21:09:28 +0100

    Reset migrations

--- MatchFM/Migrations/201611112005147_InitialMigration.Designer.cs
+++ MatchFM/Migrations/201611112005147_InitialMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace MatchFM.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class NullableAlbumReleaseDate : IMigrationMetadata
+    public sealed partial class InitialMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(NullableAlbumReleaseDate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "201611110927042_NullableAlbumReleaseDate"; }
+            get { return "201611112005147_InitialMigration"; }
         }
         
         string IMigrationMetadata.Source
--- MatchFM/Migrations/201611112005147_InitialMigration.cs
+++ MatchFM/Migrations/201611112005147_InitialMigration.cs
@@ -3,7 +3,7 @@ namespace MatchFM.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class MusicModels : DbMigration
+    public partial class InitialMigration : DbMigration
     {
         public override void Up()
         {
@@ -12,28 +12,29 @@ namespace MatchFM.Migrations
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false),
-                        MbId = c.String(maxLength: 36),
                         Image = c.String(),
-                        ReleaseDate = c.DateTime(nullable: false),
+                        ReleaseDate = c.DateTime(),
                         ArtistId = c.Int(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 200),
+                        MbId = c.String(maxLength: 36),
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Artists", t => t.ArtistId, cascadeDelete: true)
-                .Index(t => t.MbId, unique: true, name: "AlbumMbID")
-                .Index(t => t.ArtistId);
+                .Index(t => new { t.Name, t.ArtistId }, unique: true, name: "IX_Name")
+                .Index(t => t.MbId);
             
             CreateTable(
                 "dbo.Artists",
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false),
-                        MbId = c.String(maxLength: 36),
                         Image = c.String(),
+                        Name = c.String(nullable: false, maxLength: 200),
+                        MbId = c.String(maxLength: 36),
                     })
                 .PrimaryKey(t => t.Id)
-                .Index(t => t.MbId, unique: true, name: "ArtistMbID");
+                .Index(t => t.Name, unique: true)
+                .Index(t => t.MbId);
             
             CreateTable(
                 "dbo.Tags",
@@ -49,15 +50,15 @@ namespace MatchFM.Migrations
                 c => new
                     {
                         Id = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false),
-                        MbId = c.String(maxLength: 36),
                         Duration = c.Int(nullable: false),
                         AlbumId = c.Int(nullable: false),
+                        Name = c.String(nullable: false, maxLength: 200),
+                        MbId = c.String(maxLength: 36),
                     })
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.Albums", t => t.AlbumId, cascadeDelete: true)
-                .Index(t => t.MbId, unique: true, name: "TrackMbID")
-                .Index(t => t.AlbumId);
+                .Index(t => new { t.Name, t.AlbumId }, unique: true, name: "IX_Name")
+                .Index(t => t.MbId);
             
             CreateTable(
                 "dbo.UserTracks",
@@ -73,6 +74,77 @@ namespace MatchFM.Migrations
                 .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId)
                 .Index(t => t.TrackId);
+            
+            CreateTable(
+                "dbo.AspNetUsers",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Gender = c.Int(nullable: false),
+                        Photo = c.String(),
+                        Location = c.Geography(),
+                        Email = c.String(maxLength: 256),
+                        EmailConfirmed = c.Boolean(nullable: false),
+                        PasswordHash = c.String(),
+                        SecurityStamp = c.String(),
+                        PhoneNumber = c.String(),
+                        PhoneNumberConfirmed = c.Boolean(nullable: false),
+                        TwoFactorEnabled = c.Boolean(nullable: false),
+                        LockoutEndDateUtc = c.DateTime(),
+                        LockoutEnabled = c.Boolean(nullable: false),
+                        AccessFailedCount = c.Int(nullable: false),
+                        UserName = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.UserName, unique: true, name: "UserNameIndex");
+            
+            CreateTable(
+                "dbo.AspNetUserClaims",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        ClaimType = c.String(),
+                        ClaimValue = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserLogins",
+                c => new
+                    {
+                        LoginProvider = c.String(nullable: false, maxLength: 128),
+                        ProviderKey = c.String(nullable: false, maxLength: 128),
+                        UserId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.LoginProvider, t.ProviderKey, t.UserId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.AspNetUserRoles",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        RoleId = c.String(nullable: false, maxLength: 128),
+                    })
+                .PrimaryKey(t => new { t.UserId, t.RoleId })
+                .ForeignKey("dbo.AspNetUsers", t => t.UserId, cascadeDelete: true)
+                .ForeignKey("dbo.AspNetRoles", t => t.RoleId, cascadeDelete: true)
+                .Index(t => t.UserId)
+                .Index(t => t.RoleId);
+            
+            CreateTable(
+                "dbo.AspNetRoles",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(nullable: false, maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .Index(t => t.Name, unique: true, name: "RoleNameIndex");
             
             CreateTable(
                 "dbo.TagAlbums",
@@ -117,9 +189,13 @@ namespace MatchFM.Migrations
         
         public override void Down()
         {
+            DropForeignKey("dbo.AspNetUserRoles", "RoleId", "dbo.AspNetRoles");
             DropForeignKey("dbo.TagTracks", "Track_Id", "dbo.Tracks");
             DropForeignKey("dbo.TagTracks", "Tag_Name", "dbo.Tags");
             DropForeignKey("dbo.UserTracks", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserRoles", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserLogins", "UserId", "dbo.AspNetUsers");
+            DropForeignKey("dbo.AspNetUserClaims", "UserId", "dbo.AspNetUsers");
             DropForeignKey("dbo.UserTracks", "TrackId", "dbo.Tracks");
             DropForeignKey("dbo.Tracks", "AlbumId", "dbo.Albums");
             DropForeignKey("dbo.TagArtists", "Artist_Id", "dbo.Artists");
@@ -133,16 +209,28 @@ namespace MatchFM.Migrations
             DropIndex("dbo.TagArtists", new[] { "Tag_Name" });
             DropIndex("dbo.TagAlbums", new[] { "Album_Id" });
             DropIndex("dbo.TagAlbums", new[] { "Tag_Name" });
+            DropIndex("dbo.AspNetRoles", "RoleNameIndex");
+            DropIndex("dbo.AspNetUserRoles", new[] { "RoleId" });
+            DropIndex("dbo.AspNetUserRoles", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserLogins", new[] { "UserId" });
+            DropIndex("dbo.AspNetUserClaims", new[] { "UserId" });
+            DropIndex("dbo.AspNetUsers", "UserNameIndex");
             DropIndex("dbo.UserTracks", new[] { "TrackId" });
             DropIndex("dbo.UserTracks", new[] { "UserId" });
-            DropIndex("dbo.Tracks", new[] { "AlbumId" });
-            DropIndex("dbo.Tracks", "TrackMbID");
-            DropIndex("dbo.Artists", "ArtistMbID");
-            DropIndex("dbo.Albums", new[] { "ArtistId" });
-            DropIndex("dbo.Albums", "AlbumMbID");
+            DropIndex("dbo.Tracks", new[] { "MbId" });
+            DropIndex("dbo.Tracks", "IX_Name");
+            DropIndex("dbo.Artists", new[] { "MbId" });
+            DropIndex("dbo.Artists", new[] { "Name" });
+            DropIndex("dbo.Albums", new[] { "MbId" });
+            DropIndex("dbo.Albums", "IX_Name");
             DropTable("dbo.TagTracks");
             DropTable("dbo.TagArtists");
             DropTable("dbo.TagAlbums");
+            DropTable("dbo.AspNetRoles");
+            DropTable("dbo.AspNetUserRoles");
+            DropTable("dbo.AspNetUserLogins");
+            DropTable("dbo.AspNetUserClaims");
+            DropTable("dbo.AspNetUsers");
             DropTable("dbo.UserTracks");
             DropTable("dbo.Tracks");
             DropTable("dbo.Tags");
